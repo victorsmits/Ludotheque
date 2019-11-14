5a33d5fd9ed37b2031f448237a463a44660cfaec
victorsmits <victor.smits@student.vinci.be>
2019-11-14 11:02:53 +0100

    test jenkins

--- src/main/java/gamelibrary/CreateGameFactory.java
+++ src/main/java/gamelibrary/CreateGameFactory.java
@@ -4,18 +4,18 @@ public class CreateGameFactory {
 
     //use getShape method to get object of type game
     public FactoryGame getCreateGame(String gameType){
-        if(gameType == null){
-            return null;
-        }
-        if(gameType.equalsIgnoreCase("TOY")){
-            return new Toy();
-
-        } else if(gameType.equalsIgnoreCase("VideoGame")){
-            return new VideoGame();
-
-        } else if(gameType.equalsIgnoreCase("BoardGame")){
-            return new BoardGame();
-        }
+//        if(gameType == null){
+//            return null;
+//        }
+//        if(gameType.equalsIgnoreCase("TOY")){
+//            return new Toy();
+//
+//        } else if(gameType.equalsIgnoreCase("VideoGame")){
+//            return new VideoGame();
+//
+//        } else if(gameType.equalsIgnoreCase("BoardGame")){
+//            return new BoardGame();
+//        }
 
         return null;
     }
--- src/test/java/gamelibrary/AuthentificationTest.java
+++ src/test/java/gamelibrary/AuthentificationTest.java
@@ -17,75 +17,75 @@ import static org.junit.Assert.*;
  * @author ablo1
  */
 public class AuthentificationTest {
-    private static Authentification auth;
-    
-    public AuthentificationTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-        auth = new Authentification("rudy36", "1234");
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of getUsername method, of class Authentification.
-     */
-    @Test
-    public void testGetUsername() {
-
-        String expResult = "rudy34";
-        String result = auth.getUsername();
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of setUsername method, of class Authentification.
-     */
-    @Test
-    public void testSetUsername() {
-
-        String username = "rudy34";
-        auth.setUsername(username);
-        
-        String expResult = "rudy34";
-        String result = auth.getUsername();
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of getPassword method, of class Authentification.
-     */
-    @Test
-    public void testGetPassword() {
-        String expResult = "4321";
-        String result = auth.getPassword();
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of setPassword method, of class Authentification.
-     */
-    @Test
-    public void testSetPassword() {
-        System.out.println("setPassword");
-        String password = "4321";
-        auth.setPassword(password);
-        
-        String expResult = "4321";
-        String result = auth.getPassword();
-        assertEquals(expResult, result);
-    }
+//    private static Authentification auth;
+//
+//    public AuthentificationTest() {
+//    }
+//
+//    @BeforeClass
+//    public static void setUpClass() {
+////        auth = new Authentification("rudy36", "1234");
+//    }
+//
+//    @AfterClass
+//    public static void tearDownClass() {
+//    }
+//
+//    @Before
+//    public void setUp() {
+//    }
+//
+//    @After
+//    public void tearDown() {
+//    }
+//
+//    /**
+//     * Test of getUsername method, of class Authentification.
+//     */
+//    @Test
+//    public void testGetUsername() {
+//
+//        String expResult = "rudy34";
+////        String result = auth.getUsername();
+//        assertEquals(expResult, result);
+//    }
+//
+//    /**
+//     * Test of setUsername method, of class Authentification.
+//     */
+//    @Test
+//    public void testSetUsername() {
+//
+//        String username = "rudy34";
+////        auth.setUsername(username);
+//
+//        String expResult = "rudy34";
+////        String result = auth.getUsername();
+//        assertEquals(expResult, result);
+//    }
+//
+//    /**
+//     * Test of getPassword method, of class Authentification.
+//     */
+//    @Test
+//    public void testGetPassword() {
+//        String expResult = "4321";
+////        String result = auth.getPassword();
+//        assertEquals(expResult, result);
+//    }
+//
+//    /**
+//     * Test of setPassword method, of class Authentification.
+//     */
+//    @Test
+//    public void testSetPassword() {
+//        System.out.println("setPassword");
+//        String password = "4321";
+////        auth.setPassword(password);
+//
+//        String expResult = "4321";
+////        String result = auth.getPassword();
+//        assertEquals(expResult, result);
+//    }
     
 }
