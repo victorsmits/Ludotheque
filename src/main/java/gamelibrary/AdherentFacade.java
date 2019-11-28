f333eb1b27fa0edafd69264cf64013c006b1cfac
victorsmits <victor.smits@student.vinci.be>
2019-11-28 09:51:39 +0100

    checkstyle AdherentFacade

--- src/main/java/gamelibrary/AdherentFacade.java
+++ src/main/java/gamelibrary/AdherentFacade.java
@@ -1,5 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
+ * To change this license header,
+ * choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -9,4 +10,5 @@ package gamelibrary;
  *
  * @author ablo1
  */
-public interface AdherentFacade { }
+public interface AdherentFacade
+{}
